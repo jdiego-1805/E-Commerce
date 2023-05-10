f631d778242327c50ca0e30e2be9c3c39cb5f497
jdiego-1805 <63172283+jdiego-1805@users.noreply.github.com>
2023-05-09 20:30:26 -0500

    added on index.js

---
+++ Develop/models/index.js
@@ -0,0 +1,39 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+Product.belongsTo(Category,{
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+})
+
+// Categories have many Products
+
+Category.hasMany(Product,{
+foreignKey: 'product_id',
+})
+
+// Products belongToMany Tags (through ProductTag)
+
+Product.belongsToMany(Tag, {
+through: ProductTag,
+onDelete: 'SET NULL'
+})
+
+// Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+through: ProductTag,
+onDelete: 'SET NULL'
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
